ca533e55b37d6a4485a9dda04fe0be55ad23cc96
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-07 13:14:35 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { landingPageComponent } from './landingPage/landingPage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, landingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component (
+    {
+        selector: 'app-landingPage',
+        standalone: true,
+        imports : [CommonModule],
+        styleUrl: 'landingPage.component.scss',
+        templateUrl: 'landingPage.component.html'
+    }
+)
+
+export class landingPageComponent {}

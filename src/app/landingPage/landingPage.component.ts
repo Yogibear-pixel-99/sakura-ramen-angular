a81b3f0fdc10cc8480d6fc664abe6e3cb6a3da2d
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-07 15:25:28 +0200

    added navbar to section

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component (
     {
         selector: 'app-landingPage',
         standalone: true,
-        imports : [CommonModule],
+        imports : [CommonModule, NavbarComponent],
         styleUrl: 'landingPage.component.scss',
         templateUrl: 'landingPage.component.html'
     }

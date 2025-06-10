06975a95bc7d3440f503ddefe7e4b44ec66dc16c
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-10 10:23:40 +0200

    added secitions and started designing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContent } from "./main-content/main-content.component";
-import { LandingPageComponent } from "./main-content/landing-page/landingPage.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, MainContent, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, MainContent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FindUsAtComponent } from './find-us-at/find-us-at.component';
 
-
-@Component (
-    {
-        selector: 'app-main-content',
-        standalone: true,
-        imports : [CommonModule, LandingPageComponent],
-        styleUrl: 'main-content.component.scss',
-        templateUrl: 'main-content.component.html'
-    }
-)
-
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [
+    CommonModule,
+    LandingPageComponent,
+    HowToOrderComponent,
+    OurMenuComponent,
+    FindUsAtComponent
+  ],
+  styleUrl: 'main-content.component.scss',
+  templateUrl: 'main-content.component.html',
+})
 export class MainContent {}

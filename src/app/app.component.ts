f21fd1565c59d68a114c9237ca084635287f938b
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-11 22:32:24 +0200

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { MainContent } from './main-content/main-content.component';
-import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, FooterComponent, MainContent, ImprintComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

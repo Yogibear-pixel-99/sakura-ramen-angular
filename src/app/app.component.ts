2acce17bdf3212efc1770804cbfba866c13ef4b3
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-11 12:07:11 +0200

    finished website design and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContent } from "./main-content/main-content.component";
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, MainContent, FooterComponent, ImprintComponent],
+  imports: [CommonModule, RouterOutlet, MainContent, ImprintComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
